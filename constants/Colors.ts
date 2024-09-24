fdeeab7216151789b25e1aac34398d1b81f1917f
JGabriel-SL <gabrielsl.alu.lmb@gmail.com>
2024-09-24 17:31:43 -0300

    adjusting the project

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -6,21 +6,11 @@
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
-export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+export const colors = {
+  background: '#0F232C',
+  white: "#FFF",
+  green: "#1EB500",
+  orange: "#F79600",
+  blue: "#009AD6",
+  black: "#000"
 };
